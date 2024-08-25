250256dd8fe66d0566721e78f5430dd279bbd48a
HP <samu.kauppi@metropolia.fi>
2024-08-25 14:25:46 +0300

    add tasks to 1_5

--- Teht1_2_Tilitä_nostaminen/main.cpp
+++ Teht1_2_Tilitä_nostaminen/main.cpp
@@ -1,6 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
 
+int account;
+mutex account_mutex;
+
+void editing_account(int count, int amount)
+{
+	for (int i = 0; i < count; i++)
+	{
+		lock_guard<mutex> lock(account_mutex);
+		account -= amount;
+	}
+}
 
 int main()
 {
+	const int starting_value = 100000000;
+	account = starting_value;
 
+	thread add(editing_account, starting_value, 1);
+	thread take(editing_account, starting_value, -1);
+	
+	add.join();
+	take.join();
+
+	cout << "Starting value: " << starting_value;
+	cout << "\nCurrent value: " << account;
 }
