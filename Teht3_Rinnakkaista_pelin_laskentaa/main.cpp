01d71cd9dcb39721ad868da3a46007e5d104c8eb
HP <samu.kauppi@metropolia.fi>
2024-08-25 15:32:56 +0300

    fix project naming

---
+++ Teht3_Rinnakkaista_pelin_laskentaa/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+class Game_Task
+{
+public:
+	virtual void perform() = 0;
+	virtual ~Game_Task() = default;
+};
+
+class Character : public Game_Task
+{
+private:
+	int hp = 1000000;
+public:
+	void perform() override
+	{
+		for (size_t i = 0; i < 1000000000; i++)
+		{
+			take_damage(i);
+		}
+	}
+
+	void take_damage(int value)
+	{
+		hp -= value;
+	}
+};
+
+class Game_Map : public Game_Task
+{
+public:
+	void perform() override
+	{
+		for (size_t i = 0; i < 10000000; i++)
+		{
+			int x = i * i * i;
+		}
+	}
+};
+
+
+int perform_with_threads(vector<Game_Task*>& tasks)
+{
+	int max_threads = thread::hardware_concurrency() - 1;
+
+	if (max_threads < 1)
+		return -1;
+
+	auto t1 = chrono::high_resolution_clock::now();
+	vector<thread> threads;
+	for (size_t i = 0; i < tasks.size(); i++)
+	{
+		if (threads.size() >= max_threads)
+		{
+			threads[i % max_threads].join();
+		}
+
+		threads.emplace_back(&Game_Task::perform, tasks[i]);
+	}
+	for (auto& t : threads)
+	{
+		if (t.joinable())
+		{
+			t.join();
+		}
+	}
+	auto t2 = chrono::high_resolution_clock::now();
+	return chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+}
+
+int perform_without_threads(vector<Game_Task*>& tasks)
+{
+	auto t3 = chrono::high_resolution_clock::now();
+	for (size_t i = 0; i < tasks.size(); i++)
+	{
+		tasks[i]->perform();
+	}
+	auto t4 = chrono::high_resolution_clock::now();
+	return chrono::duration_cast<chrono::milliseconds>(t4 - t3).count();
+}
+
+int main()
+{
+	vector<Game_Task*> tasks;
+
+	tasks.push_back(new Character());
+	tasks.push_back(new Character());
+	tasks.push_back(new Game_Map());
+	tasks.push_back(new Game_Map());
+
+
+	// Measure time taken without threads
+	int time1 = perform_without_threads(tasks);
+	cout << "Without threading: " << time1 << " ms\n";
+
+	// Measure time taken with threads
+	int time2 = perform_with_threads(tasks);
+	cout << "With threading: " << time2 << " ms\n";
+
+	// Clean up allocated memory
+	for (auto task : tasks)
+	{
+		delete task;
+	}
+	return 0;
+}
---
+++ Teht4_Summa_asynkronisilla_funktiokutsuilla/main.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <future>
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+void calculate_partial_sum(vector<int>& nums, int& result, size_t start, size_t end)
+{
+	result = accumulate(nums.begin() + start, nums.begin() + end, 0);
+}
+
+
+int main()
+{
+
+	// M‰‰ritell‰‰n numeroiden m‰‰r‰
+	const int num_count = 100000;
+
+	// M‰‰ritell‰‰n s‰ikeiden m‰‰r‰t ja numeroiden arvot
+	const size_t thread_count = thread::hardware_concurrency();
+	vector<int> nums(num_count, 1);
+
+	// Vaihtoehtoinen satunnainen arvojen m‰‰ritys
+	//srand(time(0));
+	//for (int& num : nums)
+	//{
+	//	num = rand() % 11;
+	//}
+
+	vector<future<void>> futures;							// M‰‰ritell‰‰n s‰ikeet vektoriin
+	vector<int> results(thread_count);						// M‰‰ritell‰‰n osasummien palautus vektori
+	size_t elements_in_threads = num_count / thread_count;	// M‰‰ritell‰‰n osasummien m‰‰r‰
+
+	for (size_t i = 0; i < thread_count; i++)
+	{
+		// M‰‰ritell‰‰n seuraavan s‰ikeen aloitus ja lopetus piste
+		size_t start = i * elements_in_threads;
+		size_t end = (i == thread_count - 1) ? num_count : start + elements_in_threads;
+
+		// Aloiteaan funktiokutsu s‰ikeess‰
+		futures.emplace_back(async(launch::async, calculate_partial_sum, ref(nums), ref(results[i]), start, end));
+	}
+
+	// Odotetaan, ett‰ s‰ikeet ovat suorittaneet loppuun
+	for (auto& thread : futures)
+	{
+		thread.get();
+	}
+
+	int sum = accumulate(results.begin(), results.end(), 0);
+
+	cout << "Laskettujen arvojen summa: " << sum << "\n";
+	cout << "Arvojen m‰‰r‰: " << num_count;
+}
